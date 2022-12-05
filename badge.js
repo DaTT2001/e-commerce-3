a0758bf54f8a811c4e814ab859c3c337097615cf
Duong Cong Dat <104434271+DaTT2001@users.noreply.github.com>
2022-12-05 19:03:37 +0700

    Update badge.js

--- badge.js
+++ badge.js
@@ -1,9 +1,18 @@
 const badge1 = document.querySelector(".badge-component")
 console.log(badge1);
-const oldListJson = localStorage.getItem('cart');
-const existingList = JSON.parse(oldListJson)
-let num = 0
-for(let i = 0; i < existingList.length; i++) {
-    num += Number(existingList[i].quantity)
-}
-badge1.textContent = num
+fetch(
+    "https://e-commerce-90793-default-rtdb.firebaseio.com/cart.json"
+)     
+    .then((response) => response.json())
+    .then((data) => {
+        const output = []; 
+        for(const id in data) {
+            output.push(data[id]);
+        }
+        let num = 0
+        for(let i= 0; i<output.length; i++) {
+            num += Number(output[i].quantity)
+        }
+        badge1.textContent = num
+    }
+    )
